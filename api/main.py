4ffb288bd8345f1c88346371392ffd7d0382bf57
lioncat2002 <sankhayan2002@gmail.com>
2022-07-03 06:48:14 +0530

    refactor: fix formatting with black

--- api/main.py
+++ api/main.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI, File, UploadFile
 
-app=FastAPI()
+app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"This is": "a test"}
 
+
 @app.post("/upload/")
-async def upload(file:UploadFile):
-    contents=await file.read()#--> file is a standard python file object
-    #TODO: Add a function call here to the ML model which will accept the file object
+async def upload(file: UploadFile):
+    contents = await file.read()  # --> file is a standard python file object
+    # TODO: Add a function call here to the ML model which will accept the file object
 
-    #temporary code to write image on to the disk
-    with open("tmp/img.jpeg","wb") as f:
+    # temporary code to write image on to the disk
+    with open("tmp/img.jpeg", "wb") as f:
         f.write(contents)
-    return {"filename":file.filename} # Possibly make it so that this single endpoint returns caption?
-
+    return {
+        "filename": file.filename
+    }  # Possibly make it so that this single endpoint returns caption?
