a3b8aa7c2659bb7718ce2cad737f79c4b3d31e9c
lioncat2002 <sankhayan2002@gmail.com>
2022-07-04 14:04:08 +0530

    fix: api now accepts only images

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, File, UploadFile
-
+from fastapi import FastAPI, UploadFile
+import os
 app = FastAPI()
 
 
@@ -11,11 +11,15 @@ def read_root():
 @app.post("/upload/")
 async def upload(file: UploadFile):
     contents = await file.read()  # --> file is a standard python file object
+    if not file.filename.lower().endswith(('.jpg','.jpeg','.png','.bmp','.tiff')):
+        return {'error':'File is not an image'} 
     # TODO: Add a function call here to the ML model which will accept the file object
-
+    
     # temporary code to write image on to the disk
+    if not os.path.exists('tmp'):
+           os.makedirs('tmp')
     with open("tmp/img.jpeg", "wb") as f:
         f.write(contents)
-    return {
-        "filename": file.filename
-    }  # Possibly make it so that this single endpoint returns caption?
+        return {
+            "filename": file.filename
+        }  # Possibly make it so that this single endpoint returns caption?
