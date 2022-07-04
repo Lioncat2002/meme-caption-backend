580b65aae9d9d988938a90a7005f81457cfd53d6
lioncat2002 <sankhayan2002@gmail.com>
2022-07-04 15:09:28 +0530

    chore: fix formatting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, UploadFile
 import os
+
+from fastapi import FastAPI, UploadFile
+
 app = FastAPI()
 
 
@@ -11,15 +13,14 @@ def read_root():
 @app.post("/upload/")
 async def upload(file: UploadFile):
     contents = await file.read()  # --> file is a standard python file object
-    if not file.filename.lower().endswith(('.jpg','.jpeg','.png','.bmp','.tiff')):
-        return {'error':'File is not an image'} 
+    if not file.filename.lower().endswith((".jpg", ".jpeg", ".png", ".bmp", ".tiff")):
+        return {"error": "File is not an image"}
     # TODO: Add a function call here to the ML model which will accept the file object
-    
+
     # temporary code to write image on to the disk
-    if not os.path.exists('tmp'):
-           os.makedirs('tmp')
+    if not os.path.exists("tmp"):
+        os.makedirs("tmp")
     with open("tmp/img.jpeg", "wb") as f:
         f.write(contents)
-        return {
-            "filename": file.filename
-        }  # Possibly make it so that this single endpoint returns caption?
+        return {"filename": file.filename}
+        # Possibly make it so that this single endpoint returns caption?
