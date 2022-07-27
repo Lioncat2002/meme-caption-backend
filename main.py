e729d90f1b4ca50e94f771af2201d559c6b37639
lioncat2002 <sankhayan2002@gmail.com>
2022-07-28 02:29:57 +0530

    fix: converting file to binary

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ def read_root():
 @app.post("/upload/")
 async def upload(file:bytes=File()):#: bytes = File()):
     # TODO: Pass the base64 file to the ml model
-    with open("tmp/test.png", "wb") as fo:
-        fo.write(file)
-    return {"file": "file uploaded"}
-    # contents = file.file.read()
-    # if not file.filename.lower().endswith((".jpg", ".jpeg", ".png", ".bmp", ".tiff")):
-    #    return {"error": "File is not an image"}
-    ## TODO: Add a function call here to the ML model which will accept the file object
-    # f = io.TextIOWrapper(
-    #    file.file
-    # )  # to convert the UploadFile into a python file object
-    ## temporary code to write image on to the disk
-    # if not os.path.exists("tmp"):
-    #    os.makedirs("tmp")
-    # with open("tmp/img.jpeg", "wb") as f:
-    #    f.write(contents)
+    #with open("tmp/test.png", "wb") as fo:
+    #    fo.write(file)
+    #return {"file": "file uploaded"}
+     contents = file
+     #if not file.filename.lower().endswith((".jpg", ".jpeg", ".png", ".bmp", ".tiff")):
+     #   return {"error": "File is not an image"}
+    # TODO: Add a function call here to the ML model which will accept the file object
+     f = io.TextIOWrapper(
+        file
+     )  # to convert the UploadFile into a python file object
+    # temporary code to write image on to the disk
+     if not os.path.exists("tmp"):
+        os.makedirs("tmp")
+     with open("tmp/img.jpeg", "wb") as f:
+        f.write(contents)
 
-    #    return {"filename": file.filename}
+        return {"filename": file.filename}
     # Possibly make it so that this single endpoint returns caption?
