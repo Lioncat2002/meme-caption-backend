7dc5d7ade830460fe0f2432d28653174e3fdd0c8
lioncat2002 <sankhayan2002@gmail.com>
2022-07-27 06:07:09 +0530

    fix: fixing the upload formats

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,7 @@
 import requests
 
-url = "https://memer-ai.herokuapp.com/upload/"
-# url = "http://127.0.0.1:8000/upload/"
-file = {"file": open("images/noita.jpg", "rb")}
-resp = requests.post(url=url, files=file)
+url = "https://memer-ai.herokuapp.com/upload/mrow"
+url = "http://127.0.0.1:8000/upload/mrow"
+#file = {"file": open("images/noita.jpg", "rb")}
+resp = requests.get(url=url)
 print(resp.json())
