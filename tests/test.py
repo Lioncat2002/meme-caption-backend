fcec2b11c31ad2693d233eba1a3a4d0b1953d45c
lioncat2002 <sankhayan2002@gmail.com>
2022-07-28 01:10:00 +0530

    fix: converting file to binary

--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,6 @@ import base64
 import pickle
 url = f"https://memer-ai.herokuapp.com/upload/"
 url = "http://127.0.0.1:8000/upload/"
-file = {"file": base64.urlsafe_b64encode(open("images/noita.jpg", "rb").read())}
+file = {"file": open("images/noita.jpg", "rb").read()}
 resp = requests.post(url=url, files=file)
 print(resp.text)
