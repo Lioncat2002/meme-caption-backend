19c9bf8d0a3bfe8c1fb5e9144296cd2472865af6
lioncat2002 <sankhayan2002@gmail.com>
2022-07-28 00:51:04 +0530

    fix: working on file upload

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,8 @@
 import requests
-
-url = "https://memer-ai.herokuapp.com/upload/"
-# url = "http://127.0.0.1:8000/upload/"
-file = {"file": open("images/noita.jpg", "rb")}
+import base64
+import pickle
+url = f"https://memer-ai.herokuapp.com/upload/"
+url = "http://127.0.0.1:8000/upload/"
+file = {"file": base64.urlsafe_b64encode(open("images/noita.jpg", "rb").read())}
 resp = requests.post(url=url, files=file)
-print(resp.json())
+print(resp.text)
