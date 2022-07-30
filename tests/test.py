bee84ea2dd9b8cf04866b56b428909ac32df6e63
Lioncat2002 <sankhayan2002@gmail.com>
2022-07-30 19:27:49 +0530

    chore: added cors

--- tests/test.py
+++ tests/test.py
@@ -6,10 +6,10 @@ headers = {
     # Already added when you pass json= but not when you pass data=
     # 'Content-Type': 'application/json',
 }
-s=base64.b64encode(open("images/noita.jpg","rb").read())
+s=open("images/noita.jpg","rb")
 
 json_data = {
-    'data': s.decode('utf-8'),
+    'data': s,
 }
 
 response = requests.post('http://127.0.0.1:8000/encoded/', headers=headers, json=json_data)
