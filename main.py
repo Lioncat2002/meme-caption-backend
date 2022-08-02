a186fe0465cd8dd1970c7dea45f126b8b76e4c2c
Lioncat2002 <sankhayan2002@gmail.com>
2022-08-02 22:09:56 +0530

    refactor: removed dead code

--- main.py
+++ main.py
@@ -19,68 +19,32 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class ImageData(BaseModel):
-    data:str
+    data: str
 
 
 @app.get("/")
 def read_root():
     return {"This is": "a test"}
 
-@app.post("/encoded/")
-async def base64data(data:ImageData):
-    print("Success")
-    d=base64.b64decode(data.data.encode('utf-8'))
-    f=io.BytesIO(d)
-    f=Image.open(f) #PIL image
-    #f.show()
-    if not os.path.exists("tmp"):
-        os.makedirs("tmp")
-    with open("tmp/t.png","wb") as fs:
-        fs.write(d)
-    #f=io.BytesIO(data)
-    return {"got":"ze file"}
-
-    
 
 @app.post("/image/")
-async def imageupload(data:UploadFile=File(...)):
+async def imageupload(data: UploadFile = File(...)):
     print("Success")
-    
-    #checking if the uplaoded file is image or not
+
+    # checking if the uplaoded file is image or not
     if not data.filename.lower().endswith((".jpg", ".jpeg", ".png", ".bmp", ".tiff")):
         return {"error": "File is not an image"}
 
-    f=data.file
-    #Temporary code to write image to drive
+    f = data.file
+    # Temporary code to write image to drive
     if not os.path.exists("tmp"):
         os.makedirs("tmp")
-    with open("tmp/image.jpg","wb") as fs:
+    with open("tmp/image.jpg", "wb") as fs:
         fs.write(f.read())
-    f=Image.open(f)
-    transform=transforms.Compose([
-        transforms.PILToTensor()
-    ])
-    img_tensor=transform(f)
-    #f.show()
-    return {"got":"success"}
-
-@app.post("/upload/")
-async def upload(file:bytes=File()):#: bytes = File()):
-    # TODO: Pass the base64 file to the ml model
-    #with open("tmp/test.png", "wb") as fo:
-    #    fo.write(file)
-    #return {"file": "file uploaded"}
-     contents = file
-     #if not file.filename.lower().endswith((".jpg", ".jpeg", ".png", ".bmp", ".tiff")):
-     #   return {"error": "File is not an image"}
-    # TODO: Add a function call here to the ML model which will accept the file object
-     
-    # temporary code to write image on to the disk
-     if not os.path.exists("tmp"):
-        os.makedirs("tmp")
-     with open("tmp/img.jpeg", "wb") as f:
-        f.write(contents)
-
-     return {"file": "uploaded"}
-    # Possibly make it so that this single endpoint returns caption?
+    f = Image.open(f)
+    transform = transforms.Compose([transforms.PILToTensor()])
+    img_tensor = transform(f)
+    print(img_tensor)
+    return {"got": "sucess"}
--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,14 @@
 import requests
-import base64
-from PIL import Image
+
 headers = {
-    'accept': 'application/json',
-    # Already added when you pass json= but not when you pass data=
-    # 'Content-Type': 'application/json',
-}
-s=base64.b64encode(open("images/noita.jpg","rb").read())
-
-json_data = {
-    'data': s.decode('utf-8'),
+    "accept": "application/json",
+    # requests won't add a boundary if this header is set when you pass files=
+    # 'Content-Type': 'multipart/form-data',
 }
 
-response = requests.post('http://127.0.0.1:8000/encoded/', headers=headers, json=json_data)
+files = {
+    "data": open("images/noita.jpg", "rb"),
+}
+
+response = requests.post("http://127.0.0.1:8000/image/", headers=headers, files=files)
 print(response.text)
